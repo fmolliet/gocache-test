663a01d56ba3b77f911e35cd8599ea42446e720a
F.Molliet <fmolliet@hotmail.com>
2020-02-20 14:05:23 -0300

    Primeiro commit

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+skt = socket.socket(socket.AF_INET)
+skt.connect(('127.0.0.7', 8000))
+skt.sendall('OK')
+
+while True:
+    data = skt.recv(512)
+    if ( len(data) < 1 ) :
+        break
+    print (data);
+
+skt.close()        
